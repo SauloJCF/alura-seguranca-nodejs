0f28f4f158a3dace13abc266699b0e8b3795f60e
Saulo.Figueiredo <saulojcf@gmail.com>
2025-08-24 14:45:48 -0300

    Completa crud de usuários

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -16,6 +16,60 @@ class UsuarioController {
         }
     }
 
+    static async buscarTodosUsuarios(req, res) {
+        try {
+            const usuarios = await usuarioService.buscarTodosUsuarios();
+
+            res.status(200).send(usuarios);
+        } catch (error) {
+            res.status(400).send({ message: error.message });
+        }
+    }
+
+    static async buscarUsuarioPorId(req, res) {
+        try {
+            const id = req.params.id;
+
+            const usuario = await usuarioService.buscarUsuarioPorId(id);
+
+            if (!usuario) {
+                return res.status(404).send({ message: 'Usuário não encontrado!' });
+            }
+
+            res.status(200).send(usuario);
+        } catch (error) {
+            res.status(400).send({ message: error.message });
+        }
+    }
+
+    static async editarUsuario(req, res) {
+        try {
+            const id = req.params.id;
+            const { nome, email } = req.body;
+
+            const usuario = await usuarioService.editarUsuario({id,  nome, email });
+
+            res.status(200).send(usuario);
+        } catch (error) {
+            res.status(400).send({ message: error.message });
+        }
+    }
+
+    static async deletarUsuario(req, res) {
+        try {
+            const id = req.params.id;
+
+            const usuarioDeletado = await usuarioService.deletarUsuario(id);
+
+            if (!usuarioDeletado) {
+                return res.status(404).send({ message: 'Usuário não encontrado!' });
+            }
+
+            res.status(200).send({ message: 'Usuário deletado com sucesso!' });
+        } catch (error) {
+            res.status(400).send({ message: error.message });
+        }
+    }
 }
 
 module.exports = UsuarioController;
--- api/services/usuarioService.js
+++ api/services/usuarioService.js
@@ -9,7 +9,7 @@ class UsuarioService {
             where: {
                 email: dto.email
             }
-        });        
+        });
 
         if (usuario) {
             throw new Error('E-mail já cadastrado!');
@@ -17,19 +17,53 @@ class UsuarioService {
 
         try {
             const hashSenha = await hash(dto.senha, 8);
-    
+
             const novoUsuario = await database.usuarios.create({
                 id: uuid.v4(),
                 nome: dto.nome,
                 email: dto.email,
                 senha: hashSenha
             });
-    
-            return novoUsuario;            
+
+            return novoUsuario;
         } catch (error) {
             throw new Error('Erro ao cadastrar usuário!');
         }
 
+    }
+
+    async buscarTodosUsuarios() {
+        const usuarios = await database.usuarios.findAll();
+        return usuarios;
+    }
+
+    async buscarUsuarioPorId(id) {
+        const usuario = await database.usuarios.findByPk(id);
+        return usuario;
+    }
+
+    async editarUsuario(dto) {
+        const usuario = await this.buscarUsuarioPorId(dto.id)
+        try {
+            usuario.nome = dto.nome
+            usuario.email = dto.email
+
+            await usuario.save()
+            
+            return usuario
+        } catch (error) {
+            throw new Error('Erro ao editar usuario!')
+        }
+    }
+
+    async deletarUsuario(id) {
+        const usuariosDeletados = await database.usuarios.destroy({
+            where: {
+                id: id
+            }
+        });
+
+        return usuariosDeletados > 0;
     }
 }
 
