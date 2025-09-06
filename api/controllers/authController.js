8f2d0c8960031f246862a3870ea44ac826add93e
Saulo.Figueiredo <saulojcf@gmail.com>
2025-09-06 14:16:34 -0300

    Rota de login P1

---
+++ api/controllers/authController.js
@@ -0,0 +1,19 @@
+const AuthService = require('../services/authService');
+
+const authService = new AuthService();
+
+class ProdutoController {
+    static async login(req, res) {
+        const { email, senha } = req.body;
+        try {
+            const login = await authService.login({ email, senha });
+            
+            res.status(200).json(login);
+            
+        } catch (error) {
+            res.status(401).send({ message: error.message });
+        }
+    }
+}
+
+module.exports = ProdutoController;
