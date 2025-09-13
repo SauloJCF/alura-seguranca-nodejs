72d24a767ba1968205a2ac3c21d08f87ea41ddd1
Saulo.Figueiredo <saulojcf@gmail.com>
2025-09-13 13:18:35 -0300

    Vinculação de usuários a permissões pt2

--- api/services/segurancaService.js
+++ api/services/segurancaService.js
@@ -1,9 +1,44 @@
+const { Sequelize } = require('sequelize');
 const database = require('../models');
 
 class SegurancaService {
     async cadastrarAcl(dto) {
-        const usuario = await database.usuarios.findOne({
-            includes: [
+        const usuario = await this.getUsuario(dto.usuarioId);
+
+        if (!usuario) {
+            throw new Error("Usuário não cadastrado!");
+        }
+
+        const rolesCadastradas = await database.roles.findAll({
+            where: {
+                id: {
+                    [Sequelize.Op.in]: dto.roles,
+                }
+            }
+        });
+
+        const permissoesCadastradas = await database.permissoes.findAll({
+            where: {
+                id: {
+                    [Sequelize.Op.in]: dto.permissoes,
+                }
+            }
+        });
+
+        await usuario.removeUsuario_roles(usuario.usuario_roles);
+        await usuario.removeUsuario_permissoes(usuario.usuario_permissoes);
+
+        await usuario.addUsuario_roles(rolesCadastradas);
+        await usuario.addUsuario_permissoes(permissoesCadastradas);
+
+        const usuarioAtualizado = await this.getUsuario(dto.usuarioId);
+
+        return usuarioAtualizado;
+    }
+
+    async getUsuario(usuarioId) {
+        return await database.usuarios.findOne({
+            include: [
                 {
                     model: database.roles,
                     as: 'usuario_roles',
@@ -16,9 +51,9 @@ class SegurancaService {
                 },
             ],
             where: {
-                id: dto.usuarioId
+                id: usuarioId
             }
-        })
+        });
     }
 }
 
